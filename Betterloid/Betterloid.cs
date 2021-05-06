0012d725ee0edc16dcc3a91150cdb89147390b46
SeleDreams <seledreams.contact@gmail.com>
2021-05-06 08:08:12 +0200

    Fixed newtonsoft json bug

--- Betterloid/Betterloid.cs
+++ Betterloid/Betterloid.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.IO;
 using Yamaha.VOCALOID.VOCALOID5;
-using HarmonyLib;
 using System.Reflection;
-using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Threading;
+using System.Linq;
 
 namespace Betterloid
 {
     public class Betterloid
     {
-        public Harmony Harmony { get; protected set; }
         public BetterloidConfig Config { get; protected set; }
+        public Assembly NewtonsoftJSON { get; protected set; }
         public List<Plugin> StartupPlugins { get; protected set; }
         public List<Plugin> EditorPlugins { get; protected set; }
-
         public static Betterloid Instance { get; protected set; }
-
 
         private Betterloid()
         {
@@ -24,18 +24,38 @@ namespace Betterloid
             {
                 throw new TypeInitializationException(nameof(Betterloid), new Exception("An instance of Betterloid already exists!"));
             }
-            Harmony = new Harmony("com.vocaloid.patch");
             Instance = this;
             StartupPlugins = new List<Plugin>();
             EditorPlugins = new List<Plugin>();
         }
 
+        public static T DeserializeObject<T>(string json)
+        {
+            Type jsonconvert = Instance.NewtonsoftJSON.GetType("Newtonsoft.Json.JsonConvert");
+            var deserializer = jsonconvert.GetMethods(BindingFlags.Public | BindingFlags.Static)
+        .Where(i => i.Name.Equals("DeserializeObject", StringComparison.InvariantCulture))
+        .Where(i => i.IsGenericMethod)
+        .Where(i => i.GetParameters().Select(a => a.ParameterType).SequenceEqual(new Type[] { typeof(string) }))
+        .Single();
+            var genericMethod = deserializer.MakeGenericMethod(new Type[] { typeof(T) });
+            return (T)genericMethod.Invoke(null, new object[] { json });
+        }
+
+        public static string SerializeObject<T>(T source)
+        {
+            Type jsonconvert = Instance.NewtonsoftJSON.GetType("Newtonsoft.Json.JsonConvert");
+            var serializer = jsonconvert.GetMethods(BindingFlags.Public | BindingFlags.Static)
+        .Where(i => i.Name.Equals("SerializeObject", StringComparison.InvariantCulture))
+        .Where(i => i.GetParameters().Select(a => a.ParameterType).SequenceEqual(new Type[] { typeof(object) }))
+        .Single();
+            return (string)serializer.Invoke(null, new object[] { source });
+        }
 
         private void Initialize()
         {
-            Config = JsonConvert.DeserializeObject<BetterloidConfig>(File.ReadAllText("Betterloid.json"));
+            NewtonsoftJSON = Assembly.LoadFrom("Newtonsoft.Json.dll");
+            Config = DeserializeObject<BetterloidConfig>(File.ReadAllText("Betterloid.json"));
         }
-
 
         public void InitializePlugins()
         {
@@ -43,7 +63,7 @@ namespace Betterloid
             foreach (string directory in directories)
             {
                 string json = File.ReadAllText(directory + "/PluginConfig.json");
-                PluginConfig pluginConfig = JsonConvert.DeserializeObject<PluginConfig>(json);
+                PluginConfig pluginConfig = DeserializeObject<PluginConfig>(json);
 
                 Assembly asm = Assembly.LoadFrom(directory + "/" + pluginConfig.PluginAssembly);
                 string typename = string.IsNullOrEmpty(pluginConfig.PluginNamespace) ? pluginConfig.PluginClass : pluginConfig.PluginNamespace + "." + pluginConfig.PluginClass;
@@ -56,7 +76,7 @@ namespace Betterloid
 
                 IPlugin plugin = (IPlugin)Activator.CreateInstance(t);
 
-                if ( pluginConfig.PluginType == "Startup")
+                if (pluginConfig.PluginType == "Startup")
                 {
                     StartupPlugins.Add(new Plugin(plugin, pluginConfig));
                 }
@@ -66,8 +86,6 @@ namespace Betterloid
                 }
             }
         }
-
-
         [STAThread]
         public static void Main(string[] args)
         {
@@ -75,8 +93,27 @@ namespace Betterloid
             Betterloid betterloid = new Betterloid();
             betterloid.Initialize();
             betterloid.InitializePlugins();
-            betterloid.Harmony.PatchAll();
-            App.Main();
+            App app = new App();
+            app.InitializeComponent();
+
+            app.Activated += (object sender, EventArgs arg) =>
+            {
+                Window win = app.MainWindow;
+                foreach (Plugin plugin in Instance.StartupPlugins)
+                {
+                    try
+                    {
+                        plugin.Instance.Startup();
+                    }
+                    catch
+                    {
+                        MessageBoxDeliverer.GeneralWarning("An error occurred while starting the plugin : " + plugin.Config.PluginName);
+                    }
+                    App.DoEvents();
+                }
+
+            };
+            app.Run();
         }
     }
 }
